629e304ce511a047aaa7db19bd703e17e8b47b43
Glitch (upbeat-generated-grass) <none>
2021-04-06 22:48:03 +0000

    👹🙎 Updated with Glitch

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    TOKEN: 'NjgyMzI0NTI2NzA0MzYxNDcz.XlbWPA.3x13beUIQy7Fwer-LajZSPnVdwg', // Bot Token ( You Can Get From https://discord.com/developers/applications )    
+    PREFIX: '.', 
+    STATUSMSG: 'Nitro'
+}
